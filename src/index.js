4758a44c82bd01d99fd4499ad98101822859ebda
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-12 14:29:02 +0600

    Added store, Provider and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import musicReducers from './store/reducers/musicReducer';
+
+
+const store = createStore(musicReducers, applyMiddleware(thunkMiddleware));
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
 
