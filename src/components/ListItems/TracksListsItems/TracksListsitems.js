c6fa00c30b68987951df862273045a38c0e296fe
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-16 18:08:46 +0600

    HW#86 Added props.clicked to track card

--- src/components/ListItems/TracksListsItems/TracksListsitems.js
+++ src/components/ListItems/TracksListsItems/TracksListsitems.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './TracksListsItems.css';
 
 const TracksListsItems = props => {
+    
     return (
-        <div className='track-card'>
+        <div className='track-card' onClick={props.clicked}>
             <div className='track-info-box'>
                 <h2>{props.name}</h2>
                 <p className='tracks-duration'>{props.duration}</p>
