243c54d0a4c128ec9e7654ce46854ade09b44745
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-16 18:10:35 +0600

    HW#86 Added combineReducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware ,combineReducers} from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import musicReducers from './store/reducers/musicReducer';
+import userReducer from './store/reducers/userReducer';
+
+const rootReducer = combineReducers({
+  music: musicReducers,
+  user: userReducer,
+});
 
 
-const store = createStore(musicReducers, applyMiddleware(thunkMiddleware));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 const app = (
   <Provider store={store}>
