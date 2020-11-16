067506f6b53aa051a84a4c4233789a4bb4f93980
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-16 18:06:26 +0600

    Added SignIn and SignUp Links

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,21 +3,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 import './Layout.css';
 import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 const Layout = props => {
+    const user = useSelector(state => state.user.user);
     return (
         <>
             <header className='main-header'>
-                <NavLink to='/'>
-                    <div className='logo-box'>
-                        <FontAwesomeIcon id='logo' icon={faPlayCircle} size='5x' />
-                        <h2 className='logo-title'>Music App</h2>
+                <div className='header-content'>
+                    <NavLink to='/music'>
+                        <div className='logo-box'>
+                            <FontAwesomeIcon id='logo' icon={faPlayCircle} size='5x' />
+                            <h2 className='logo-title'>Music App</h2>
+                        </div>
+                    </NavLink>
+                    <div className='users-menu'>
+                        {user == null ?
+                            <>
+                                <NavLink to='/user/register'>Sign Up</NavLink>
+                                <NavLink to='/user/login'>Sign In</NavLink>
+                            </> :
+                            <>
+                                <NavLink to='/trackHistory'>Track History</NavLink>
+                            </>}
+
                     </div>
-                </NavLink>
+                </div>
+
+
+
             </header>
             {props.children}
         </>
     );
 };
 
-export default Layout;
+export default withRouter(Layout);
