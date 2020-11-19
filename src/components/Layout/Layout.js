d26fa3ff6f6fd61efe140e4f8a10272e4076d7b6
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-19 13:24:42 +0600

    HW#87 Added Log out button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 import './Layout.css';
 import { NavLink } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUser } from '../../store/actions/userActions';
 
 
 const Layout = props => {
     const user = useSelector(state => state.user.user);
+    const dispatch = useDispatch();
+
+    const logout = () =>{
+        dispatch(logoutUser());
+    };
     return (
         <>
             <header className='main-header'>
@@ -20,13 +27,15 @@ const Layout = props => {
                         </div>
                     </NavLink>
                     <div className='users-menu'>
-                        {user == null ?
+                        {user.length === 0 ?
                             <>
                                 <NavLink to='/user/register'>Sign Up</NavLink>
                                 <NavLink to='/user/login'>Sign In</NavLink>
                             </> :
                             <>
+                                <h4><FontAwesomeIcon icon={faUser} />Hello , {user.username}!</h4>
                                 <NavLink to='/trackHistory'>Track History</NavLink>
+                                <NavLink to='/music' onClick={logout}>Log Out</NavLink>
                             </>}
 
                     </div>
