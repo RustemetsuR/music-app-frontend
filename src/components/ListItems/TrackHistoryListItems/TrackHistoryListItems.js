b73ac7420b6c3b86196cdd1b8ecce67b0755d2db
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-19 13:25:35 +0600

    HW#87 Added datetime

--- src/components/ListItems/TrackHistoryListItems/TrackHistoryListItems.js
+++ src/components/ListItems/TrackHistoryListItems/TrackHistoryListItems.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './TrackHistoryListItems.css';
 
 const TrackHistoryListItems = props => {
+
+    const date = props.dateTime;
     return (
         <div className='track-history-list-item'>
             <h3>Track: {props.track}</h3>
             <h3>Artist: {props.artist}</h3>
-            <span>Duration: {props.duration}</span>
+            <p>Duration: {props.duration}</p>
+            <span>{date.slice(0, 10)} {date.slice(11,19)}</span>
+            
         </div>
     );
 };
