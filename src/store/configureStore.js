0fd0383dc9c6edcdba14e606aeb38c759f7e1408
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-20 15:32:07 +0600

    HW#87 Added connectRouter, routerMiddleware, createBrowserHistory

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,25 +1,36 @@
-import { createStore, applyMiddleware ,combineReducers} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from "redux-thunk";
 import musicReducers from './reducers/musicReducer';
 import userReducer from './reducers/userReducer';
-import {loadFromLocalStorage, saveToLocalStorage} from './localStorage';
+import { loadFromLocalStorage, saveToLocalStorage } from './localStorage';
+import { createBrowserHistory } from 'history';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+
+
+export const history = createBrowserHistory();
 
 const rootReducer = combineReducers({
-    music: musicReducers,
-    user: userReducer,
-  });
-  
-  const persistedState = loadFromLocalStorage();
-  
-  
-  const store = createStore(rootReducer,persistedState, applyMiddleware(thunkMiddleware));
-  
-  store.subscribe(() =>{
-    saveToLocalStorage({
-      user: {
-        user: store.getState().user.user
-      }
-    });
-  })
+  music: musicReducers,
+  user: userReducer,
+  router: connectRouter(history)
+});
 
-  export default store;
+const middleWare = [
+  thunkMiddleware,
+  routerMiddleware(history),
+];
+
+const persistedState = loadFromLocalStorage();
+
+
+const store = createStore(rootReducer, persistedState, applyMiddleware(...middleWare));
+
+store.subscribe(() => {
+  saveToLocalStorage({
+    user: {
+      user: store.getState().user.user
+    }
+  });
+})
+
+export default store;
