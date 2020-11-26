026889b54746c2596b22368f3505dcfd371d4f66
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-26 18:48:05 +0600

    Added forms routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Artists from './containers/Music/MainPage/Artists/Artists';
 import Container from './components/Container/Container';
 import Albums from './containers/Music/Albums/Albums';
@@ -9,6 +9,9 @@ import Layout from './components/Layout/Layout';
 import TrackHistory from './containers/Users/TrackHistory/TrackHistory';
 import Login from './containers/Users/Login/Login';
 import Register from './containers/Users/Register/Register';
+import AddArtist from './containers/AddNewItems/AddArtist/AddArtist';
+import AddAlbum from './containers/AddNewItems/AddAlbum/AddAlbum';
+import AddTrack from './containers/AddNewItems/AddTrack/AddTrack';
 
 const App = () => {
   return (
@@ -16,13 +19,15 @@ const App = () => {
       <Switch>
         <Container>
           <Layout>
-            <Redirect from='/' to='/music'/>
+            <Route exact path='/music' component={Artists} />
             <Route path='/user/login' component={Login}/>
             <Route path='/user/register/' component={Register} />
+            <Route path='/addArtist' component={AddArtist}/>
+            <Route path='/addAlbum' component={AddAlbum}/>
+            <Route path='/addTrack' component={AddTrack}/>
             <Route exact path='/music/:artistAlbums' component={Albums} />
             <Route path='/music/:artistAlbums/:albumTracks' component={Tracks} />
             <Route path='/trackHistory' component={TrackHistory} />
-            <Route exact path='/music' component={Artists} />
           </Layout>
         </Container>
       </Switch>
