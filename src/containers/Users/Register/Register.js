dca9b95c1af4b9ecdeb5bc19f3cd474ca3923292
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-12-03 16:27:04 +0600

    Added facebook login. Added Display name and image inputs

--- src/containers/Users/Register/Register.js
+++ src/containers/Users/Register/Register.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { register } from '../../../store/actions/userActions';
 import './Register.css';
 import '../Login/Login.css';
+import FacebookLogin from '../../../components/FacebookLogin/FacebookLogin';
 
 const Register = () => {
     const dispatch = useDispatch();
@@ -10,38 +11,67 @@ const Register = () => {
     const [registerData, setRegisterData] = useState({
         userName: '',
         password: '',
+        image: '',
+        displayName: '',
     });
 
-     const userNameOnChange = event =>{
-         const value = event.target.value;
-         const registerDataCopy = {...registerData, userName: value}
-         setRegisterData(registerDataCopy);
-     };
-
-     const passwordOnChange = event =>{
+    const userNameOnChange = event => {
         const value = event.target.value;
-        const registerDataCopy = {...registerData, password: value}
+        const registerDataCopy = { ...registerData, userName: value }
         setRegisterData(registerDataCopy);
     };
 
-    const registerSubmit = event =>{
-       event.preventDefault();
+    const dataOnChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+
         const data = {
-            username: registerData.userName,
-            password: registerData.password,
+            ...registerData,
+            [name]: value,
         };
+        console.log(registerData)
+        setRegisterData(data);
+    }
+
+    const passwordOnChange = event => {
+        const value = event.target.value;
+        const registerDataCopy = { ...registerData, password: value }
+        setRegisterData(registerDataCopy);
+    };
+
+    const registerSubmit = event => {
+        event.preventDefault();
+        let data;
+        if (registerData.image === '') {
+            data = {
+                username: registerData.userName,
+                password: registerData.password,
+                displayName: registerData.displayName,
+            };
+        }else{
+            data = {
+                username: registerData.userName,
+                password: registerData.password,
+                displayName: registerData.displayName,
+                image: registerData.image
+            };
+        }
+
         dispatch(register(data));
     };
 
     return (
         <div className='register-box user-sign-boxes'>
-            <h2>Create your account</h2> 
+            <h2>Create your account</h2>
             <div>
                 <form onSubmit={registerSubmit}>
-                    <input placeholder='Username' onChange={userNameOnChange} value={registerData.userName} required/>
-                    <input placeholder='Password' type='password' onChange={passwordOnChange} value={registerData.password} required/>
+                    <input placeholder='Username' name='userName' onChange={dataOnChange} value={registerData.userName} required />
+                    <input placeholder='Password' name='password' type='password' onChange={dataOnChange} value={registerData.password} required />
+                    <input placeholder='Image' name='image' onChange={dataOnChange} value={registerData.image} />
+                    <input placeholder='Display Name' name='displayName' onChange={dataOnChange} value={registerData.displayName} />
                     <button type='submit'>Sign Up</button>
                 </form>
+                <FacebookLogin />
             </div>
         </div>
     );
