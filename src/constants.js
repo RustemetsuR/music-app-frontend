cbb157a4a81de5a274a3b89a16e1a65dca5dc228
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-12-14 17:34:33 +0600

    Added port for tests

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
-export const apiURL = "http://localhost:8000";
+let port = 8000;
+if(process.env.REACT_APP_NODE_ENV === "test"){
+    port = 8010;
+}
+export const apiURL = "http://localhost:" + port;
 export const fbAppId = "310932736689445";
